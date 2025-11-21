32413df6698e4f4584a777576e9ba65af6b1d46f
HIDEKI <hideki@r3776.jp>
2025-11-21 14:26:37 +0900

    Remove commented-out section in __init__.py

--- src/experiments_sp/sp_robustness/__init__.py
+++ src/experiments_sp/sp_robustness/__init__.py
@@ -5,11 +5,3 @@ Experiments:
     - SP-41: Layout × Topology Robustness (multiple layouts)
     - SP-42: k-NN Parameter Robustness (effect of k on SP_adj)
 """
-```
-
----
-
-### ファイル 8/8: `outputs_sp/.gitkeep`
-```
-# Placeholder for SP experiment outputs
-# This directory will be populated by experiment scripts
