4660bccac5a8ffc56513b12ccafe725692cb0c07
HIDEKI <hideki@r3776.jp>
2025-11-21 14:25:57 +0900

    Create __init__.py for robustness experiments
    Add initial documentation for robustness validation experiments.

---
+++ src/experiments_sp/sp_robustness/__init__.py
@@ -0,0 +1,15 @@
+"""Robustness Validation Experiments.
+
+Experiments:
+    - SP-40: Dimension/N Robustness (effect of N on SP patterns)
+    - SP-41: Layout × Topology Robustness (multiple layouts)
+    - SP-42: k-NN Parameter Robustness (effect of k on SP_adj)
+"""
+```
+
+---
+
+### ファイル 8/8: `outputs_sp/.gitkeep`
+```
+# Placeholder for SP experiment outputs
+# This directory will be populated by experiment scripts
