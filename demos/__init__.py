b178baf83615fa02d98038dbaf8013891aab3812
HIDEKI <hideki@r3776.jp>
2025-11-22 23:41:32 +0900

    Create __init__.py

---
+++ demos/__init__.py
@@ -0,0 +1,10 @@
+"""Demo applications for SP measurement system.
+
+This directory contains practical demonstrations of SP metrics
+in real-world scenarios.
+
+Demos:
+    dr_evaluation: Dimensionality reduction method comparison
+"""
+
+__version__ = "1.0.0"
