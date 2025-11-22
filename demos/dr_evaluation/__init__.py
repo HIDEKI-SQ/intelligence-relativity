4534551bf2cd595f3c7a94e3d41de9461f380251
HIDEKI <hideki@r3776.jp>
2025-11-22 23:40:56 +0900

    Create __init__.py

---
+++ demos/dr_evaluation/__init__.py
@@ -0,0 +1,11 @@
+"""Dimensionality Reduction Evaluation Demo.
+
+This demo shows how to use SP metrics to evaluate different
+dimensionality reduction methods (t-SNE, UMAP, PCA).
+
+Author: HIDEKI
+Date: 2025-11
+License: MIT
+"""
+
+__version__ = "1.0.0"
